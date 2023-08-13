1a1f6f79552995e14264b0ec615925249cebb8f5
Schrixx <fun_nos@yahoo.com>
2023-08-12 21:17:55 -0700

    style: change page colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,10 @@ export default {
   theme: {
     extend: {
       colors: {
-        darkGray: "#222831",
-        hover: "#606C80",
-        blueishGray: "#393E46",
-        creamYellow: "#FFD369",
-        darkenedWhite: "#EEEEEE",
+        raisinBlack: "#221E22",
+        lighterRaisinBlack: "#373137",
+        hyundaiYellow: "#ECA72C",
+        whiteSmoke: "#F2F2F2",
       }
     },
   },
