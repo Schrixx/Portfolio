8eb53be4cefc94b013efd22d2c77a05c5ee29dbb
Schrixx <fun_nos@yahoo.com>
2023-08-14 17:09:09 -0700

    refactor: small nav changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,35 +2,32 @@ import { NavLink } from "react-router-dom"
 import { navbarData } from "src/data/navbarData"
 import { v4 as uuidv4 } from 'uuid'
 import { AiOutlineMenu } from 'react-icons/ai'
-import { useEffect, useState } from "react"
+import { SlArrowRight } from "react-icons/sl"
+import { useState } from "react"
 
 
 const Navbar = () => {
 
   const [mobileMenu, setMobileMenu] = useState<boolean>(false)
 
-  useEffect(() => {
-    console.log(mobileMenu)
-  }, [mobileMenu])
-
   const manageMobileMenu = () => {
     setMobileMenu(mobileMenu => !mobileMenu)
   }
 
   return (
-    <header className="fixed z-40 w-full flex justify-center">
+    <header className={`fixed z-50 w-full flex flex-col justify-center`}>
 
-      {/* main nav menu */}
+      {/* main nav */}
       <nav className="w-full lg:w-auto border-lighterRaisinBlack border-b-[1px] lg:border-x-[1px] border-t-0">
-        <div className={`sm:hidden flex justify-between py-5 px-7 w-full ${mobileMenu ? "" : "shadow-lg"}`}>
+        <div className={`sm:hidden flex justify-between py-5 px-7 w-full ${mobileMenu ? "" : "shadow-lg"} transition-shadow duration-500`}>
           <p className="text-whiteSmoke tracking-widest text-base">Moses Osuna</p>
           <button onClick={manageMobileMenu}><AiOutlineMenu color="#EEEEEE" size="22px" /></button>
         </div>
         <ul className="hidden sm:flex sm:justify-center list-none w-full shadow-lg">
           {navbarData.map((item) => {
             return (
-              <li key={uuidv4()} className="flex flex-grow flex-shrink border-r-[1px] last:border-r-0 border-lighterRaisinBlack first:bg-lighterRaisinBlack">
-                <NavLink to={item.path} className="py-4 lg:pl-7 lg:pr-[12vw] pl-5 transition-colors duration-500 text-whiteSmoke hover:text-hyundaiYellow">
+              <li key={uuidv4()} className="flex flex-grow lg:flex-grow-0 first-letter:flex-shrink border-r-[1px] last:border-r-0 border-lighterRaisinBlack first:bg-lighterRaisinBlack">
+                <NavLink to={item.path} className="p-4 lg:pl-7 lg:pr-[12vw] pl-5 transition-colors duration-500 text-whiteSmoke hover:text-hyundaiYellow">
                   <span className="tracking-widest text-base">{item.title}</span>
                 </NavLink>
               </li>
@@ -40,19 +37,24 @@ const Navbar = () => {
       </nav>
 
       {/* mobile menu */}
-      <nav className={`w-full ${mobileMenu ? "sm:opacity-0 opacity-100" : "opacity-0"} transition-opacity delay-75 duration-500`}>
-        <ul className={`${mobileMenu ? "absolute top-[65px] z-50 sm:hidden" : "top-[-100%]"} left-0 w-full flex flex-col justify-center transition-all duration-1000`}>
-          {navbarData.map((item) => {
-              return (
-                <li key={uuidv4()} className="">
-                  <NavLink to={item.path} className=" transition-colors duration-500 text-whiteSmoke hover:text-hyundaiYellow">
-                    <span className="tracking-widest">{item.title}</span>
-                  </NavLink>
-                </li>
-              )
-            })} 
-        </ul>
-      </nav>
+      <div className={`relative z-40 w-full bg-raisinBlack h-fit ${mobileMenu ? "sm:opacity-0 opacity-100" : "opacity-0"} transition-opacity duration-300`}>
+        <nav className={`absolute ${mobileMenu ? 'sm:left-[-100%] top-0' : 'left-[-100%]'} left-0 bg-raisinBlack border-b-[1px] py-8 border-lighterRaisinBlack w-full transition-all duration-700 shadow-lg`}>
+          <ul className="flex flex-col items-center w-full overflow-y-scroll">
+            {navbarData.map((item) => {
+                return (
+                  <li key={uuidv4()} className="flex items-center w-full py-4">
+                    <div className="h-px w-5 bg-hyundaiYellow" />
+                    <NavLink to={item.path} onClick={manageMobileMenu} className="pl-4 py-4 w-full transition-colors duration-500 text-whiteSmoke hover:text-hyundaiYellow">
+                      <span className="tracking-wider">{item.title}</span>
+                    </NavLink>
+                    <span className="pr-6"><SlArrowRight size="20px" color="#ECA72C" /></span>
+                  </li>
+                )
+              })} 
+          </ul>
+        </nav>          
+      </div>
+
     </header>
   )
 }
