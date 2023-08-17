a31d6005c16d839be548bbbff8177868866feecd
Schrixx <fun_nos@yahoo.com>
2023-08-16 21:54:59 -0700

    refactor: minor changes to color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = () => {
           <button onClick={manageMobileMenu}><AiOutlineMenu color="#EEEEEE" size="22px" /></button>
         </div>
         <ul className="hidden sm:flex list-none w-full lg:max-w-fit lg:mx-auto shadow-lg lg:border-x-[1px] lg:border-b-[1px] border-lighterRaisinBlack">
-          {navbarData.map((item: any) => {
+          {navbarData.map((item) => {
             return (
               <li key={uuidv4()} className="flex flex-grow lg:flex-grow-0 lg:flex-shrink border-r-[1px] last:border-r-0 border-lighterRaisinBlack first:bg-lighterRaisinBlack">
                 <NavLink to={item.path} className="p-4 lg:pl-7 lg:pr-[13vw] pl-5 transition-colors duration-500 text-whiteSmoke hover:text-hyundaiYellow">
@@ -40,10 +40,10 @@ const Navbar = () => {
       <div className={`relative z-40 w-full bg-raisinBlack h-fit ${mobileMenu ? "sm:opacity-0 opacity-100" : "opacity-0"} transition-opacity duration-300`}>
         <nav className={`absolute ${mobileMenu ? 'sm:left-[-100%] top-0' : 'left-[-100%]'} left-0 bg-raisinBlack border-b-[1px] py-8 border-lighterRaisinBlack w-full transition-all duration-700 shadow-lg`}>
           <ul className="flex flex-col items-center w-full overflow-y-scroll">
-            {navbarData.map((item: any) => {
+            {navbarData.map((item) => {
                 return (
                   <li key={uuidv4()} className="flex items-center w-full py-4">
-                    <div className="h-px w-5 bg-hyundaiYellow" />
+                    <div className="h-px w-5 bg-lighterHyundai" />
                     <NavLink to={item.path} onClick={manageMobileMenu} className="pl-4 py-4 w-full transition-colors duration-500 text-whiteSmoke hover:text-hyundaiYellow">
                       <span className="tracking-wider">{item.title}</span>
                     </NavLink>
