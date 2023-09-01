60f626df23cbf32e37fdc3750991126629bf211e
Schrixx <fun_nos@yahoo.com>
2023-08-31 23:36:09 -0700

    refactor: add framer motion animations

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,14 @@
+import { useState } from "react"
 import { NavLink } from "react-router-dom"
+
 import Layout from "src/components/Layout"
-import { v4 as uuidv4 } from 'uuid'
+import { RevealLeft, RevealRight, RevealStaggered, RevealVertical } from "src/components/RevealAnimations"
+
+import { v4 as uuidv4 } from "uuid"
 
 import { IconContext } from "react-icons"
 import { BsCode } from "react-icons/bs"
 import { HiCode } from "react-icons/hi"
-// import { TiDocumentText } from "react-icons/ti"
 import { RiComputerLine } from "react-icons/ri"
 import { HiOutlineMail } from "react-icons/hi"
 import { BsGithub } from "react-icons/bs"
@@ -21,7 +24,6 @@ import violin from "assets/portfolioImages/violin.jpg"
 import cactus from "assets/portfolioImages/cactus.jpg"
 
 import { companyWebsiteSkills1, companyWebsiteSkills2, skillsData } from "src/data/skillsData"
-import { useState } from "react"
 
 const Home = () => {
   const [showAbout, setShowAbout] = useState<boolean>(false)
@@ -39,9 +41,9 @@ const Home = () => {
       <div id="intro" className="pt-[65px] lg:pt-60" />
 
       {/* intro */}
-      <div id="intro" className="px-4 lg:px-8 text-whiteSmoke">
+      <div className="px-4 lg:px-8 text-whiteSmoke">
         <div className="xl:max-w-7xl xl:mx-auto flex flex-col lg:flex-row justify-between">
-          <div className="flex flex-col gap-4 md:gap-6 max-w-2xl order-2 lg:order-1">
+          <RevealLeft className="flex flex-col gap-4 md:gap-6 max-w-2xl order-2 lg:order-1">
             <div className="flex justify-center sm:justify-normal items-center gap-2">
               <div className="h-px w-9 bg-bittersweet" />
               <h1 className="text-bittersweet pixelFont sm:text-3xl lg:text-4xl tracking-widest">Web Developer</h1>
@@ -54,15 +56,15 @@ const Home = () => {
                 {/* <button className="bg-darkerBittersweet hover:bg-bittersweet rounded-lg p-3 sm:p-4 w-fit transition-colors flex items-center scale-90 sm:scale-100"><span className="mr-4"><TiDocumentText color="#F2F2F2" size="24" /></span>Resume</button> */}
               </div>
             </div>
-          </div>
-          <div className="flex justify-center items-center order-1 lg:order-2 my-16 lg:my-0">
+          </RevealLeft>
+          <RevealRight className="flex justify-center items-center order-1 lg:order-2 my-16 lg:my-0">
             <div className="flex lg:flex-col items-center gap-4">
               <div className="bg-darkerBittersweet w-10 sm:w-36 lg:w-2 h-2 lg:h-36 mb-2" />
               <NavLink to="https://github.com/Schrixx" target="_blank" data-tooltip="Github" className="md:tooltip"><img src={githubIcon} className="h-16 w-full" /></NavLink>
               <NavLink to="https://www.linkedin.com/in/moses-osuna-a7501b9a/" target="_blank" data-tooltip="LinkedIn" className="md:tooltip"><img src={linkedInIcon} className="h-16 w-full" /></NavLink>
               <div className="bg-darkerBittersweet w-10 sm:w-36 lg:w-2 h-2 lg:h-36 mb-2" />
             </div>
-          </div>
+          </RevealRight>
         </div>
       </div>
 
@@ -70,14 +72,17 @@ const Home = () => {
 
       {/* skills */}
       <div id="skills" className="px-4 lg:px-8 xl:px-0 xl:max-w-7xl xl:mx-auto text-whiteSmoke flex flex-col gap-4">
-        <p className="tracking-widest">The Skills and Tools That Bring My Work To Life</p>
+        <RevealVertical><p className="tracking-widest">The Skills and Tools That Bring My Work To Life</p></RevealVertical>
         <ul className="flex flex-wrap gap-4">
-          {skillsData.map((item) => {
+          {skillsData.map((item, index) => {
             return (
-              <li key={uuidv4()} className="flex items-center gap-2 bg-lighterRaisinBlack p-2 rounded-lg lg:hover:scale-105 transition-transform">
-                <img src={item.icon} className="h-7 w-auto" />
-                <p className="tracking-wide cursor-default">{item.title}</p>
-              </li>
+              <RevealStaggered key={uuidv4()} index={index}>
+                <li className="flex items-center gap-2 bg-lighterRaisinBlack p-2 rounded-lg lg:hover:scale-105 transition-transform">
+                  <img src={item.icon} className="h-7 w-auto" />
+                  <p className="tracking-wide cursor-default">{item.title}</p>
+                </li>
+              </RevealStaggered>
+
             )
           })}
         </ul>
@@ -87,18 +92,18 @@ const Home = () => {
 
       {/* portfolio */}
       <div className="px-4 lg:px-8 xl:max-w-7xl xl:mx-auto text-whiteSmoke flex flex-col gap-20">
-        <div className="flex flex-col items-center justify-center gap-4">
+        <RevealVertical className="flex flex-col items-center justify-center gap-4">
           <h1 className="text-4xl font-semibold">
             Portfolio
           </h1>
           <p className="text-lighterHyundai text-center portfolioScreen:text-start">
             Check out my projects using video demonstrations, links to source code, and live websites.
           </p>
-        </div>
+        </RevealVertical>
         {/* 1 */}
         <div className="flex flex-wrap lg:flex-row lg:items-center justify-center portfolioScreen:justify-between gap-4 px-4 sm:px-0">
-          <div className="relative w-full pb-[56.25%] sm:pb-[40%] portfolioScreen:pb-[29%] h-0 max-w-[560px] order-2 portfolioScreen:order-1 shadow-negHard shadow-lighterHyundai"><iframe width="560" height="315" className="absolute top-0 left-0 w-full h-full" src="https://www.youtube.com/embed/YgzISpOb-MY?si=BVB57nhSI7Bl63Zu" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe></div>
-          <div className="flex flex-col gap-4 max-w-lg order-1 portfolioScreen:order-2">
+          <RevealLeft className="relative w-full pb-[56.25%] sm:pb-[40%] portfolioScreen:pb-[29%] h-0 max-w-[560px] order-2 portfolioScreen:order-1 shadow-negHard shadow-lighterHyundai"><iframe width="560" height="315" className="absolute top-0 left-0 w-full h-full" src="https://www.youtube.com/embed/YgzISpOb-MY?si=BVB57nhSI7Bl63Zu" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe></RevealLeft>
+          <RevealRight className="flex flex-col gap-4 max-w-lg order-1 portfolioScreen:order-2">
             <div className="flex items-center gap-2">
               <h1 className="text-2xl font-semibold">
                 1. Responsive Company Website
@@ -119,12 +124,14 @@ const Home = () => {
             </div>
             <h1 className="text-2xl font-semibold">Tech Used</h1>
             <ul className="flex flex-wrap gap-4">
-              {companyWebsiteSkills1.map((item) => {
+              {companyWebsiteSkills1.map((item, index) => {
                 return (
-                  <li key={uuidv4()} className="flex items-center gap-2 bg-lighterRaisinBlack p-1 rounded-lg lg:hover:scale-105 transition-transform">
-                    <img src={item.icon} className="h-5 w-auto" />
-                    <p className="tracking-wide cursor-default text-lg">{item.title}</p>
-                  </li>
+                  <RevealStaggered key={uuidv4()} index={index}>
+                    <li className="flex items-center gap-2 bg-lighterRaisinBlack p-1 rounded-lg lg:hover:scale-105 transition-transform">
+                      <img src={item.icon} className="h-5 w-auto" />
+                      <p className="tracking-wide cursor-default text-lg">{item.title}</p>
+                    </li>
+                  </RevealStaggered>
                 )
               })}
             </ul>
@@ -134,15 +141,15 @@ const Home = () => {
                 <NavLink to="https://landing-page-schrixx.vercel.app/" target="_blank" className="pt-2 tooltip" data-tooltip="Live Website"><button><RiComputerLine size="36" /></button></NavLink>
               </div>
             </IconContext.Provider>
-          </div>
+          </RevealRight>
         </div>
         {/* 2 */}
         <div className="my-2 sm:my-4 portfolioScreen:my-0" />
         <div className="flex flex-wrap lg:flex-row lg:items-center justify-center portfolioScreen:justify-between gap-4 px-4 sm:px-0">
-          <div className="bg-lighterRaisinBlack w-[560px] h-[315px] flex items-center justify-center text-center order-2 shadow-hard shadow-lighterHyundai">
+          <RevealRight className="bg-lighterRaisinBlack w-[560px] h-[315px] flex items-center justify-center text-center order-2 shadow-hard shadow-lighterHyundai">
             <p className="text-2xl">Currently Under Development</p>
-          </div>
-          <div className="flex flex-col gap-4 max-w-lg order-1">
+          </RevealRight>
+          <RevealLeft className="flex flex-col gap-4 max-w-lg order-1">
             <div className="flex items-center gap-2">
               <h1 className="text-2xl font-semibold">
                 2. Portfolio Website
@@ -156,12 +163,14 @@ const Home = () => {
             </p>
             <h1 className="text-2xl font-semibold">Tech Used</h1>
             <ul className="flex flex-wrap gap-4">
-              {companyWebsiteSkills2.map((item) => {
+              {companyWebsiteSkills2.map((item, index) => {
                 return (
-                  <li key={uuidv4()} className="flex items-center gap-2 bg-lighterRaisinBlack p-1 rounded-lg lg:hover:scale-105 transition-transform">
-                    <img src={item.icon} className="h-5 w-auto" />
-                    <p className="tracking-wide cursor-default text-lg">{item.title}</p>
-                  </li>
+                  <RevealStaggered key={uuidv4()} index={index}>
+                    <li className="flex items-center gap-2 bg-lighterRaisinBlack p-1 rounded-lg lg:hover:scale-105 transition-transform">
+                      <img src={item.icon} className="h-5 w-auto" />
+                      <p className="tracking-wide cursor-default text-lg">{item.title}</p>
+                    </li>
+                  </RevealStaggered>
                 )
               })}
             </ul>
@@ -170,15 +179,15 @@ const Home = () => {
                 <NavLink to="https://github.com/Schrixx/Weather-App" target="_blank" className="pt-2 tooltip" data-tooltip="Source Code"><button><HiCode size="36" /></button></NavLink>
               </div>
             </IconContext.Provider>
-          </div>
+          </RevealLeft>
         </div>
         {/* 3 */}
         <div className="my-2 sm:my-4 portfolioScreen:my-0" />
         <div className="flex flex-wrap lg:flex-row lg:items-center justify-center portfolioScreen:justify-between gap-4 px-4 sm:px-0">
-          <div className="bg-lighterRaisinBlack w-[560px] h-[315px] flex items-center justify-center order-2 portfolioScreen:order-1 text-center shadow-negHard shadow-lighterHyundai">
+          <RevealLeft className="bg-lighterRaisinBlack w-[560px] h-[315px] flex items-center justify-center order-2 portfolioScreen:order-1 text-center shadow-negHard shadow-lighterHyundai">
             <p className="text-2xl">Currently Under Development</p>
-          </div>
-          <div className="flex flex-col gap-4 max-w-lg order-1 portfolioScreen:order-2">
+          </RevealLeft>
+          <RevealRight className="flex flex-col gap-4 max-w-lg order-1 portfolioScreen:order-2">
             <div className="flex items-center gap-2">
               <h1 className="text-2xl font-semibold">
                 3. Weather Web App
@@ -204,25 +213,23 @@ const Home = () => {
                 <NavLink to="https://github.com/Schrixx/Weather-App" target="_blank" className="pt-2 tooltip" data-tooltip="Source Code"><button><HiCode size="36" /></button></NavLink>
               </div>
             </IconContext.Provider>
-          </div>
+          </RevealRight>
         </div>
-        {/* 3 */}
-
       </div>
 
       <div id="about" className="pt-36 sm:pt-52" />
       
       {/* About */}
       <div className="px-4 aboutScreen:px-0 aboutScreen:max-w-7xl aboutScreen:mx-auto flex flex-col justify-center gap-20 text-center aboutScreen:text-start">
-        <div className="flex flex-col items-center gap-4">
+        <RevealVertical className="flex flex-col items-center gap-4">
           <h1 className="text-4xl font-semibold text-whiteSmoke">
             About Me
           </h1>
           <div className="h-px w-4/5 bg-whiteSmoke" />
-        </div>
+        </RevealVertical>
         <div className="flex flex-wrap justify-center aboutScreen:justify-between items-center gap-4 aboutScreen:gap-0">
-          <img src={aboutIMG} width="525px" height="350px" className="rounded-3xl shadow-hard" />
-          <div className="flex flex-col gap-6 max-w-xl items-center aboutScreen:items-baseline">
+          <RevealLeft><img src={aboutIMG} width="525px" height="350px" className="rounded-3xl shadow-hard" /></RevealLeft>
+          <RevealRight className="flex flex-col gap-6 max-w-xl items-center aboutScreen:items-baseline">
             <h1 className="text-whiteSmoke text-5xl sm:text-6xl font-bold tracking-wide">
               Moses Osuna
             </h1>
@@ -235,28 +242,28 @@ const Home = () => {
             </p>
             <button onClick={toggleAbout} className={`${showAbout ? "hidden" : ""} underline italic text-evenLighterRaisinBlack text-start w-fit`}>Show More...</button>
             <a href="#contact" className={`${showAbout ? "hidden": ""} scroll-smooth`}><button className="bg-caribbeanCurrent hover:bg-evenLighterRaisinBlack text-whiteSmoke rounded-lg p-2 sm:p-3 w-fit transition-colors flex items-center"><span className="mr-4"><HiOutlineMail color="#F2F2F2" size="24" /></span>Contact Me</button></a>
-          </div>
+          </RevealRight>
         </div>
         <div className={`${showAbout ? "flex": "hidden"} flex-wrap justify-center aboutScreen:justify-between items-center gap-4 aboutScreen:gap-0`}>
-          <img src={cactus} width="525px" height="350px" className="rounded-3xl shadow-hard" />
-          <div className="max-w-xl">
+          <RevealLeft><img src={cactus} width="525px" height="350px" className="rounded-3xl shadow-hard" /></RevealLeft>
+          <RevealRight className="max-w-xl">
             <p className="text-evenLighterRaisinBlack text-xl tracking-wide leading-normal">
               I was born and raised in Tucson, Arizona, always curious about technology and games as I was growing up.
               I eventually started using my parents' computer, fiddling with settings and learning how the insides work.
               This led to me taking many technology classes in highschool, and even attending college as a cybersecurity major, where I am today.
               Even after all this time, I still have loads of fun learning about tech and programming!
             </p>
-          </div>
+          </RevealRight>
         </div>
         <div className={`${showAbout ? "flex": "hidden"} flex-wrap justify-center aboutScreen:justify-between items-center gap-4 aboutScreen:gap-0`}>
-          <img src={violin} width="525px" height="350px" className="rounded-3xl shadow-hard" />
-          <div className="max-w-xl">
+          <RevealLeft><img src={violin} width="525px" height="350px" className="rounded-3xl shadow-hard" /></RevealLeft>
+          <RevealRight className="max-w-xl">
             <p className="text-evenLighterRaisinBlack text-xl tracking-wide leading-normal">
               In regard to my hobbies, I am in love with learning and my free time is never void of some form of it.
-              I love 🎨Art, keeping up with ⌨️Programming Languages, playing 🎮Games, and playing the 🎻Violin.
+              I love 🎨Art, keeping up with ⌨️Programming Languages, playing 🎮Games competitively, and playing the 🎻Violin.
               In particular, I've recently been having lots of fun with art and bought a drawing tablet to create digital art with!
             </p>
-          </div>
+          </RevealRight>
         </div>
         <div className="flex flex-col items-center gap-6">
           <button onClick={toggleAbout} className={`${showAbout ? "" : "hidden"} underline italic text-evenLighterRaisinBlack text-start w-fit`}>...Show Less</button>
@@ -267,7 +274,7 @@ const Home = () => {
       <div id="contact" className="pt-36 sm:pt-52" />
 
       {/* contact */}
-      <div className="xs:px-4">
+      <RevealVertical className="xs:px-4">
         <div className='flex flex-col lg:flex-row max-w-7xl w-fit mx-auto bg-lighterRaisinBlack shadow-smallContainer rounded-xl text-whiteSmoke shadow-xl'>
           <div className="relative bg-slightLighterRaisingBlack p-12 rounded-t-xl lg:rounded-l-xl lg:rounded-tr-none">
             <img src={backgroundLights} className="hidden lg:block absolute bottom-0 right-0 rounded-bl-xl" />
@@ -324,7 +331,7 @@ const Home = () => {
             <button className='p-2 bg-darkerBittersweet hover:bg-bittersweet text-whiteSmoke rounded-lg max-w-fit transition-colors'>Submit</button>
           </form>
         </div>
-      </div>
+      </RevealVertical>
 
       <div id="footer" className="pt-16 sm:pt-52" />
     </Layout>
